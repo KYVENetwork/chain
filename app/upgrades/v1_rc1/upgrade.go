e3afe52b63c3e636d9390926119489fdd185f0d5
John Letey <john@kyve.network>
2023-02-13 15:04:14 +0100

    chore: cleanup upgrade files

---
+++ app/upgrades/v1_rc1/upgrade.go
@@ -0,0 +1,18 @@
+package v1rc1
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	// Upgrade
+	upgradeTypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradeTypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradeTypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
