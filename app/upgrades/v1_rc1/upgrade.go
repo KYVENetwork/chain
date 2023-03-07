113746b145d735f54da593f5315a512fc843fe68
Maximilian Breithecker <72022235+mbreithecker@users.noreply.github.com>
2023-03-07 08:25:30 +0100

    chore: finalise `v1.0.0-rc1` upgrade (#11)
    Co-authored-by: John Letey <john@kyve.network>
    (cherry picked from commit 0ca75326ebaa67a1169953751b9b2fddc9bddd17)

--- app/upgrades/v1_rc1/upgrade.go
+++ app/upgrades/v1_rc1/upgrade.go
@@ -1,6 +1,7 @@
 package v1rc1
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	storeTypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -8,6 +9,10 @@ import (
 	// Bundles
 	bundlesKeeper "github.com/KYVENetwork/chain/x/bundles/keeper"
 	bundlesTypes "github.com/KYVENetwork/chain/x/bundles/types"
+	// Capability
+	capabilityKeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
+	// ICA Controller
+	v6 "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/controller/migrations/v6"
 	// Upgrade
 	upgradeTypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
@@ -16,33 +21,24 @@ func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	bundlesKeeper bundlesKeeper.Keeper,
-	bundlesStoreKey storeTypes.StoreKey,
+	cdc codec.BinaryCodec,
+	capabilityStoreKey *storeTypes.KVStoreKey,
+	capabilityKeeper *capabilityKeeper.Keeper,
+	moduleName string,
 ) upgradeTypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradeTypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		// TODO(@john): Do we need this check?
-		if ctx.ChainID() != "kaon-1" {
-			return vm, nil
+		if err := v6.MigrateICS27ChannelCapability(ctx, cdc, capabilityStoreKey, capabilityKeeper, moduleName); err != nil {
+			return nil, err
 		}
 
-		// TODO(@john): Do we need to run the ICS27 migration?
-		// We never used ICA Controller features prior to this upgrade.
+		ReinitialiseBundlesParams(ctx, bundlesKeeper)
 
-		ReinitialiseBundlesParams(ctx, bundlesKeeper, bundlesStoreKey)
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
 
 // ReinitialiseBundlesParams ...
-func ReinitialiseBundlesParams(
-	ctx sdk.Context,
-	bundlesKeeper bundlesKeeper.Keeper,
-	_ storeTypes.StoreKey,
-) {
-	// TODO(@john): Do we need to delete the old store?
-
-	// store := ctx.KVStore(bundlesStoreKey)
-	// store.Delete(bundlesTypes.ParamsKey)
-
+func ReinitialiseBundlesParams(ctx sdk.Context, bundlesKeeper bundlesKeeper.Keeper) {
 	params := bundlesTypes.DefaultParams()
 	bundlesKeeper.SetParams(ctx, params)
 }
