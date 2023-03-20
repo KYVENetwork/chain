199a36b54648ab8bbdf5c2224e4246e628fb12cd
John Letey <john@kyve.network>
2023-03-20 11:52:15 +0100

    fix(`2.2.11`): validate msg inputs

--- x/stakers/types/message_create_staker.go
+++ x/stakers/types/message_create_staker.go
@@ -2,39 +2,30 @@ package types
 
 import (
 	"cosmossdk.io/errors"
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	errorsTypes "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-const TypeMsgCreateStaker = "create_staker"
-
 var _ sdk.Msg = &MsgCreateStaker{}
-
-func (msg *MsgCreateStaker) Route() string {
-	return RouterKey
-}
-
-func (msg *MsgCreateStaker) Type() string {
-	return TypeMsgCreateStaker
-}
 
 func (msg *MsgCreateStaker) GetSigners() []sdk.AccAddress {
 	creator, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
 		panic(err)
 	}
+
 	return []sdk.AccAddress{creator}
 }
 
-func (msg *MsgCreateStaker) GetSignBytes() []byte {
-	bz := ModuleCdc.MustMarshalJSON(msg)
-	return sdk.MustSortJSON(bz)
-}
-
 func (msg *MsgCreateStaker) ValidateBasic() error {
-	_, err := sdk.AccAddressFromBech32(msg.Creator)
-	if err != nil {
-		return errors.Wrapf(errorsTypes.ErrInvalidAddress, "invalid creator address (%s)", err)
+	if _, err := sdk.AccAddressFromBech32(msg.Creator); err != nil {
+		return errors.Wrapf(errorsTypes.ErrInvalidAddress, "invalid creator address: %s", err)
 	}
+
+	if amount := math.NewIntFromUint64(msg.Amount); amount.IsNil() || amount.IsNegative() {
+		return errors.Wrapf(errorsTypes.ErrInvalidRequest, "invalid amount")
+	}
+
 	return nil
 }
--- x/stakers/types/message_join_pool.go
+++ x/stakers/types/message_join_pool.go
@@ -2,39 +2,34 @@ package types
 
 import (
 	"cosmossdk.io/errors"
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	errorsTypes "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-const TypeMsgJoinPool = "join_pool"
-
 var _ sdk.Msg = &MsgJoinPool{}
-
-func (msg *MsgJoinPool) Route() string {
-	return RouterKey
-}
-
-func (msg *MsgJoinPool) Type() string {
-	return TypeMsgJoinPool
-}
 
 func (msg *MsgJoinPool) GetSigners() []sdk.AccAddress {
 	creator, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
 		panic(err)
 	}
+
 	return []sdk.AccAddress{creator}
 }
 
-func (msg *MsgJoinPool) GetSignBytes() []byte {
-	bz := ModuleCdc.MustMarshalJSON(msg)
-	return sdk.MustSortJSON(bz)
-}
-
 func (msg *MsgJoinPool) ValidateBasic() error {
-	_, err := sdk.AccAddressFromBech32(msg.Creator)
-	if err != nil {
-		return errors.Wrapf(errorsTypes.ErrInvalidAddress, "invalid creator address (%s)", err)
+	if _, err := sdk.AccAddressFromBech32(msg.Creator); err != nil {
+		return errors.Wrapf(errorsTypes.ErrInvalidAddress, "invalid creator address: %s", err)
 	}
+
+	if _, err := sdk.AccAddressFromBech32(msg.Valaddress); err != nil {
+		return errors.Wrapf(errorsTypes.ErrInvalidAddress, "invalid validator address: %s", err)
+	}
+
+	if amount := math.NewIntFromUint64(msg.Amount); amount.IsNil() || amount.IsNegative() {
+		return errors.Wrapf(errorsTypes.ErrInvalidRequest, "invalid amount")
+	}
+
 	return nil
 }
