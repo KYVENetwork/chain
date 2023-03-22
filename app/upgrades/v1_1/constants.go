a175d7f27cff844faf74992d2ee21ac920da6763
John Letey <john@kyve.network>
2023-03-22 13:06:51 +0100

    feat: prepare `v1.1.0` upgrade

---
+++ app/upgrades/v1_1/constants.go
@@ -0,0 +1,16 @@
+package v1_1
+
+// UpgradeName is the name of this specific software upgrade used on-chain.
+const UpgradeName = "v1.1.0"
+
+// InvestorAccounts is a list of accounts from our second funding round.
+var InvestorAccounts []string
+
+const (
+	// StartTime - Sep 14th, 2023.
+	// This is 6-months after KYVE mainnet launch.
+	StartTime = 1694649600
+	// EndTime - Sep 14th, 2025.
+	// This is 2 years after StartTime.
+	EndTime = 1757808000
+)
