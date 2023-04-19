f89babb3db056cbc0d09515111bbf11ef570712b
John Letey <john@kyve.network>
2023-04-19 14:02:06 +0200

    chore: prepare `v1.2` (#31)

---
+++ app/upgrades/v1_2/constants.go
@@ -0,0 +1,10 @@
+package v1_2
+
+// UpgradeName is the name of this specific software upgrade used on-chain.
+const UpgradeName = "v1.2.0"
+
+// TestnetChainID is the Chain ID of the KYVE testnet (Kaon).
+const TestnetChainID = "kaon-1"
+
+// MainnetChainID is the Chain ID of the KYVE mainnet.
+const MainnetChainID = "kyve-1"
