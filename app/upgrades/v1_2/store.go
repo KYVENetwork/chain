812ef2a5b1fafc1f0e25e427edf5cd6d0fc2d238
John Letey <john@kyve.network>
2023-04-19 14:19:32 +0200

    chore: include PFM in store loader

--- app/upgrades/v1_2/store.go
+++ app/upgrades/v1_2/store.go
@@ -3,11 +3,19 @@ package v1_2
 import (
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	storeTypes "github.com/cosmos/cosmos-sdk/store/types"
+
+	// PFM
+	pfmTypes "github.com/strangelove-ventures/packet-forward-middleware/v6/router/types"
+	// Upgrade
 	upgradeTypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
 func CreateStoreLoader(upgradeHeight int64) baseapp.StoreLoader {
-	storeUpgrades := storeTypes.StoreUpgrades{}
+	storeUpgrades := storeTypes.StoreUpgrades{
+		Added: []string{
+			pfmTypes.StoreKey,
+		},
+	}
 
 	return upgradeTypes.UpgradeStoreLoader(upgradeHeight, &storeUpgrades)
 }
