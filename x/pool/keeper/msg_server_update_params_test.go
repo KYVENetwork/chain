e86e20198ddc80c3d358e21486c1bd2c621734f4
Troy Kessler <troy.kessler99@gmail.com>
2023-05-31 10:23:37 +0200

    added unit tests for new pool params

--- x/pool/keeper/msg_server_update_params_test.go
+++ x/pool/keeper/msg_server_update_params_test.go
@@ -1,1 +1,158 @@
-package keeper
+package keeper_test
+
+import (
+	i "github.com/KYVENetwork/chain/testutil/integration"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	// Pool
+	"github.com/KYVENetwork/chain/x/pool/types"
+	// Gov
+	govV1Types "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+)
+
+/*
+
+TEST CASES - msg_server_update_params.go
+
+* Check default params
+* Invalid authority (transaction)
+* Invalid authority (proposal)
+* Update every param at once
+* Update every param back to default value
+
+*/
+
+var _ = Describe("msg_server_update_params.go", Ordered, func() {
+	s := i.NewCleanChain()
+
+	gov := s.App().GovKeeper.GetGovernanceAccount(s.Ctx()).GetAddress().String()
+
+	minDeposit := s.App().GovKeeper.GetDepositParams(s.Ctx()).MinDeposit
+	votingPeriod := s.App().GovKeeper.GetVotingParams(s.Ctx()).VotingPeriod
+
+	delegations := s.App().StakingKeeper.GetAllDelegations(s.Ctx())
+	voter := sdk.MustAccAddressFromBech32(delegations[0].DelegatorAddress)
+
+	BeforeEach(func() {
+		s = i.NewCleanChain()
+
+		delegations := s.App().StakingKeeper.GetAllDelegations(s.Ctx())
+		voter = sdk.MustAccAddressFromBech32(delegations[0].DelegatorAddress)
+	})
+
+	AfterEach(func() {
+		s.PerformValidityChecks()
+	})
+
+	It("Check default params", func() {
+		// ASSERT
+		params := s.App().PoolKeeper.GetParams(s.Ctx())
+
+		Expect(params.GlobalMinDelegation).To(Equal(types.DefaultGlobalMinDelegation))
+	})
+
+	It("Invalid authority (transaction)", func() {
+		// ARRANGE
+		msg := &types.MsgUpdateParams{
+			Authority: i.DUMMY[0],
+			Payload:   "{}",
+		}
+
+		// ACT
+		_, err := s.RunTx(msg)
+
+		// ASSERT
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("Invalid authority (proposal)", func() {
+		// ARRANGE
+		msg := &types.MsgUpdateParams{
+			Authority: i.DUMMY[0],
+			Payload:   "{}",
+		}
+
+		proposal, _ := govV1Types.NewMsgSubmitProposal(
+			[]sdk.Msg{msg}, minDeposit, i.DUMMY[0], "",
+		)
+
+		// ACT
+		_, err := s.RunTx(proposal)
+
+		// ASSERT
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("Update every param at once", func() {
+		// ARRANGE
+		payload := `{
+			"global_min_delegation": 100000000
+		}`
+
+		msg := &types.MsgUpdateParams{
+			Authority: gov,
+			Payload:   payload,
+		}
+
+		proposal, _ := govV1Types.NewMsgSubmitProposal(
+			[]sdk.Msg{msg}, minDeposit, i.DUMMY[0], "",
+		)
+
+		vote := govV1Types.NewMsgVote(
+			voter, 1, govV1Types.VoteOption_VOTE_OPTION_YES, "",
+		)
+
+		// ACT
+		_, submitErr := s.RunTx(proposal)
+		_, voteErr := s.RunTx(vote)
+
+		s.CommitAfter(*votingPeriod)
+		s.Commit()
+
+		// ASSERT
+		updatedParams := s.App().PoolKeeper.GetParams(s.Ctx())
+
+		Expect(submitErr).NotTo(HaveOccurred())
+		Expect(voteErr).NotTo(HaveOccurred())
+
+		Expect(updatedParams.GlobalMinDelegation).To(Equal(uint64(100000000)))
+	})
+
+	It("Update every param back to default value", func() {
+		// ARRANGE
+		payload := `{
+			"global_min_delegation": 0
+		}`
+
+		msg := &types.MsgUpdateParams{
+			Authority: gov,
+			Payload:   payload,
+		}
+
+		proposal, _ := govV1Types.NewMsgSubmitProposal(
+			[]sdk.Msg{msg}, minDeposit, i.DUMMY[0], "",
+		)
+
+		vote := govV1Types.NewMsgVote(
+			voter, 1, govV1Types.VoteOption_VOTE_OPTION_YES, "",
+		)
+
+		// ACT
+		_, submitErr := s.RunTx(proposal)
+		_, voteErr := s.RunTx(vote)
+
+		s.CommitAfter(*votingPeriod)
+		s.Commit()
+
+		// ASSERT
+		updatedParams := s.App().PoolKeeper.GetParams(s.Ctx())
+
+		Expect(submitErr).NotTo(HaveOccurred())
+		Expect(voteErr).NotTo(HaveOccurred())
+
+		Expect(updatedParams.GlobalMinDelegation).To(BeZero())
+	})
+})
