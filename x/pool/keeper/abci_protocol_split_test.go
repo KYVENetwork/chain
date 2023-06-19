efb36ad00eedcfd160fad1aee4034eae796f935e
Troy Kessler <troy.kessler99@gmail.com>
2023-06-19 15:21:12 +0200

    test(x/pool): completed abci tests for x/pool module

--- x/pool/keeper/abci_protocol_split_test.go
+++ x/pool/keeper/abci_protocol_split_test.go
@@ -14,9 +14,9 @@ import (
 TEST CASES - abci.go
 
 * pool should always receive inflation funds
-* TODO: pool should split inflation funds depending on operating cost
-* TODO: pools with zero operating cost should receive nothing
-* TODO: every pool has zero operating cost
+* pool should split inflation funds depending on operating cost
+* pools with zero operating cost should receive nothing
+* every pool has zero operating cost
 
 */
 
@@ -64,5 +64,109 @@ var _ = Describe("abci.go", Ordered, func() {
 			// ASSERT
 			Expect(b2).To(BeNumerically(">", b1))
 		}
+	})
+
+	It("pool should split inflation funds depending on operating cost", func() {
+		// ARRANGE
+		s.App().PoolKeeper.AppendPool(s.Ctx(), poolTypes.Pool{
+			Name:           "PoolTest2",
+			MaxBundleSize:  100,
+			StartKey:       "0",
+			UploadInterval: 60,
+			OperatingCost:  2_000_000,
+			Protocol: &poolTypes.Protocol{
+				Version:     "0.0.0",
+				Binaries:    "{}",
+				LastUpgrade: uint64(s.Ctx().BlockTime().Unix()),
+			},
+			UpgradePlan: &poolTypes.UpgradePlan{},
+		})
+
+		// ACT
+		s.Commit()
+
+		pool1, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 0)
+		b1 := uint64(s.App().BankKeeper.GetBalance(s.Ctx(), pool1.GetPoolAccount(), globalTypes.Denom).Amount.Int64())
+
+		pool2, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 1)
+		b2 := uint64(s.App().BankKeeper.GetBalance(s.Ctx(), pool2.GetPoolAccount(), globalTypes.Denom).Amount.Int64())
+
+		// ASSERT
+		Expect(b1 * 2).To(BeNumerically("~", b2, 1))
+	})
+
+	It("pools with zero operating cost should receive nothing", func() {
+		// ARRANGE
+		s.App().PoolKeeper.AppendPool(s.Ctx(), poolTypes.Pool{
+			Name:           "PoolTest2",
+			MaxBundleSize:  100,
+			StartKey:       "0",
+			UploadInterval: 60,
+			OperatingCost:  0,
+			Protocol: &poolTypes.Protocol{
+				Version:     "0.0.0",
+				Binaries:    "{}",
+				LastUpgrade: uint64(s.Ctx().BlockTime().Unix()),
+			},
+			UpgradePlan: &poolTypes.UpgradePlan{},
+		})
+
+		// ACT
+		s.Commit()
+
+		pool1, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 0)
+		b1 := uint64(s.App().BankKeeper.GetBalance(s.Ctx(), pool1.GetPoolAccount(), globalTypes.Denom).Amount.Int64())
+
+		pool2, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 1)
+		b2 := uint64(s.App().BankKeeper.GetBalance(s.Ctx(), pool2.GetPoolAccount(), globalTypes.Denom).Amount.Int64())
+
+		// ASSERT
+		Expect(b1).To(BeNumerically(">", b2))
+		Expect(b2).To(BeZero())
+	})
+
+	It("pools with zero operating cost should receive nothing", func() {
+		// ARRANGE
+		s.App().PoolKeeper.SetPool(s.Ctx(), poolTypes.Pool{
+			Id:             0,
+			Name:           "PoolTest",
+			MaxBundleSize:  100,
+			StartKey:       "0",
+			UploadInterval: 60,
+			OperatingCost:  0,
+			Protocol: &poolTypes.Protocol{
+				Version:     "0.0.0",
+				Binaries:    "{}",
+				LastUpgrade: uint64(s.Ctx().BlockTime().Unix()),
+			},
+			UpgradePlan: &poolTypes.UpgradePlan{},
+		})
+
+		s.App().PoolKeeper.AppendPool(s.Ctx(), poolTypes.Pool{
+			Name:           "PoolTest2",
+			MaxBundleSize:  100,
+			StartKey:       "0",
+			UploadInterval: 60,
+			OperatingCost:  0,
+			Protocol: &poolTypes.Protocol{
+				Version:     "0.0.0",
+				Binaries:    "{}",
+				LastUpgrade: uint64(s.Ctx().BlockTime().Unix()),
+			},
+			UpgradePlan: &poolTypes.UpgradePlan{},
+		})
+
+		// ACT
+		s.Commit()
+
+		pool1, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 0)
+		b1 := uint64(s.App().BankKeeper.GetBalance(s.Ctx(), pool1.GetPoolAccount(), globalTypes.Denom).Amount.Int64())
+
+		pool2, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 1)
+		b2 := uint64(s.App().BankKeeper.GetBalance(s.Ctx(), pool2.GetPoolAccount(), globalTypes.Denom).Amount.Int64())
+
+		// ASSERT
+		Expect(b1).To(BeZero())
+		Expect(b2).To(BeZero())
 	})
 })
