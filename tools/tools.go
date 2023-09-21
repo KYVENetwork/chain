c04fdf3044f8b8d20ecedaee7238b903973b90fd
rapha <raphael.thurnherr1990@gmail.com>
2023-09-21 11:47:23 +0200

    chore: go mod tidy

--- tools/tools.go
+++ tools/tools.go
@@ -6,11 +6,6 @@
 package tools
 
 import (
-	_ "github.com/cosmos/gogoproto/protoc-gen-gocosmos"
-	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "mvdan.cc/gofumpt"
 )
