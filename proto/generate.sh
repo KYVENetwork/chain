089ddbff612282f1ac9934cebeca32fc1513da19
rapha <raphael.thurnherr1990@gmail.com>
2023-10-18 15:31:24 +0200

    chore: revert to old generate.sh

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,22 +1,10 @@
-# set arg1 to "ts" to generate typescript code
-
-cd proto || exit 1
-
-if [ "$1" = "ts" ]; then
-  echo "Generating typescript code"
-  template="buf.gen.ts.yaml"
-else
-  echo "Generate go code and docs"
-  template="buf.gen.yaml"
-fi
-
-buf generate --template $template
+cd proto
+buf generate
 cd ..
 
-if [ "$1" != "ts" ]; then
-  cp -r github.com/KYVENetwork/chain/* ./
-  swagger-combine ./docs/config.json -o ./docs/swagger.yml
-  rm -rf tmp-swagger-gen
-fi
-
+cp -r github.com/KYVENetwork/chain/* ./
 rm -rf github.com
+
+swagger-combine ./docs/config.json -o ./docs/swagger.yml
+rm -rf tmp-swagger-gen
+d
