2866707c57f0d6ed11a2a8c7ca31d718374b1943
mbreithecker <m@breithecker.de>
2023-10-27 16:19:58 +0200

    feat: params upgrade message + new min_funding_multiple param

--- x/funders/types/params.go
+++ x/funders/types/params.go
@@ -7,15 +7,18 @@ import (
 const (
 	// DefaultMinFundingAmount 1000 Kyve
 	DefaultMinFundingAmount = uint64(1_000_000_000)
-	// DefaultMinFundingAmountPerBundle 1 Kyve
-	DefaultMinFundingAmountPerBundle = uint64(1_000_000)
+	// DefaultMinFundingAmountPerBundle 0.1 Kyve
+	DefaultMinFundingAmountPerBundle = uint64(100_000)
+	// DefaultMinFundingMultiple 20
+	DefaultMinFundingMultiple = uint64(20)
 )
 
 // NewParams creates a new Params instance
-func NewParams(minFundingAmount uint64, minFundingAmountPerBundle uint64) Params {
+func NewParams(minFundingAmount uint64, minFundingAmountPerBundle uint64, minFundingMultiple uint64) Params {
 	return Params{
 		MinFundingAmount:          minFundingAmount,
 		MinFundingAmountPerBundle: minFundingAmountPerBundle,
+		MinFundingMultiple:        minFundingMultiple,
 	}
 }
 
@@ -24,12 +27,17 @@ func DefaultParams() Params {
 	return NewParams(
 		DefaultMinFundingAmount,
 		DefaultMinFundingAmountPerBundle,
+		DefaultMinFundingMultiple,
 	)
 }
 
 // Validate validates the set of params
 func (p *Params) Validate() error {
 	if err := util.ValidateNumber(p.MinFundingAmount); err != nil {
+		return err
+	}
+
+	if err := util.ValidateNumber(p.MinFundingAmountPerBundle); err != nil {
 		return err
 	}
 
