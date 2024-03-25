0ad1447eeb0054e7d57c9f50e9198fab44d29ebc
rapha <raphael.thurnherr1990@gmail.com>
2024-03-25 11:51:46 +0100

    feat: add new protobuf generation (draft)

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,9 +1,15 @@
 cd proto
-buf generate
+buf generate --template buf.gen.gogo.yaml
+buf generate --template buf.gen.pulsar.yaml
 cd ..
 
 cp -r github.com/KYVENetwork/chain/* ./
 rm -rf github.com
+rm -rf kyve
+
+exit 0
+
+# TODO: fix docs
 
 swagger-combine ./docs/config.json -o ./docs/swagger.yml
 rm -rf tmp-swagger-gen
