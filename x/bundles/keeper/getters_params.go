47d8b522dea13d9f65d16a81f3605927539fb02c
rapha <dev@uupi.cloud>
2024-04-22 13:37:37 +0200

    refactor: use 'collections.Item' for bundle params

--- x/bundles/keeper/getters_params.go
+++ x/bundles/keeper/getters_params.go
@@ -3,20 +3,15 @@ package keeper
 import (
 	"cosmossdk.io/math"
 	"github.com/KYVENetwork/chain/x/bundles/types"
-	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // GetParams returns the current x/bundles module parameters.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
-	store := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
-
-	bz := store.Get(types.ParamsKey)
-	if bz == nil {
-		return params
+	params, err := k.BundlesParams.Get(ctx)
+	if err != nil {
+		return types.DefaultParams()
 	}
-
-	k.cdc.MustUnmarshal(bz, &params)
 	return params
 }
 
@@ -42,7 +37,5 @@ func (k Keeper) GetMaxPoints(ctx sdk.Context) (res uint64) {
 
 // SetParams sets the x/bundles module parameters.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
-	store := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
-	bz := k.cdc.MustMarshal(&params)
-	store.Set(types.ParamsKey, bz)
+	_ = k.BundlesParams.Set(ctx, params)
 }
