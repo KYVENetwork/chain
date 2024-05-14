f9e7737eac5ea2f176b422910dba3ad438579332
Troy Kessler <troy.kessler99@gmail.com>
2024-05-14 08:55:42 +0200

    test: added tests for multiple coin funding

--- x/bundles/keeper/keeper_suite_funding_bundles_test.go
+++ x/bundles/keeper/keeper_suite_funding_bundles_test.go
@@ -25,9 +25,9 @@ TEST CASES - funding bundles
 * Produce a valid bundle although multiple funders with same amount can not pay for the bundle reward
 * Produce a valid bundle although multiple funders with different amount can not pay for the bundle reward
 * Produce a valid bundle although there are no funders at all
-* TODO: Produce a valid bundle with only one funder and multiple coins
-* TODO: Produce a valid bundle with multiple funders and multiple coins
-* TODO: Produce a valid bundle although the only funder can not pay for the full bundle reward
+* Produce a valid bundle with only one funder and multiple coins
+* Produce a valid bundle with multiple funders and multiple coins
+* Produce a valid bundle although the only funder can not pay for the full bundle reward
 
 */
 
@@ -768,5 +768,253 @@ var _ = Describe("funding bundles", Ordered, func() {
 		// assert total pool funds
 		Expect(s.App().FundersKeeper.GetTotalActiveFunding(s.Ctx(), fundingState.PoolId).IsZero()).To(BeTrue())
 		Expect(fundingState.ActiveFunderAddresses).To(HaveLen(0))
+	})
+
+	It("Produce a valid bundle with only one funder and multiple coins", func() {
+		// ARRANGE
+		s.RunTxFundersSuccess(&funderstypes.MsgFundPool{
+			Creator:          i.ALICE,
+			PoolId:           0,
+			Amounts:          sdk.NewCoins(i.ACoin(100*i.T_KYVE), i.BCoin(50*i.T_KYVE)),
+			AmountsPerBundle: sdk.NewCoins(i.ACoin(10*i.T_KYVE), i.BCoin(2*i.T_KYVE)),
+		})
+
+		s.CommitAfterSeconds(60)
+
+		s.RunTxBundlesSuccess(&bundletypes.MsgSubmitBundleProposal{
+			Creator:       i.VALADDRESS_0_A,
+			Staker:        i.STAKER_0,
+			PoolId:        0,
+			StorageId:     "y62A3tfbSNcNYDGoL-eXwzyV-Zc9Q0OVtDvR1biJmNI",
+			DataSize:      100,
+			DataHash:      "test_hash",
+			FromIndex:     0,
+			BundleSize:    100,
+			FromKey:       "0",
+			ToKey:         "99",
+			BundleSummary: "test_value",
+		})
+
+		s.RunTxBundlesSuccess(&bundletypes.MsgVoteBundleProposal{
+			Creator:   i.VALADDRESS_1_A,
+			Staker:    i.STAKER_1,
+			PoolId:    0,
+			StorageId: "y62A3tfbSNcNYDGoL-eXwzyV-Zc9Q0OVtDvR1biJmNI",
+			Vote:      1,
+		})
+
+		s.CommitAfterSeconds(60)
+
+		// ACT
+		s.RunTxBundlesSuccess(&bundletypes.MsgSubmitBundleProposal{
+			Creator:       i.VALADDRESS_1_A,
+			Staker:        i.STAKER_1,
+			PoolId:        0,
+			StorageId:     "P9edn0bjEfMU_lecFDIPLvGO2v2ltpFNUMWp5kgPddg",
+			DataSize:      100,
+			DataHash:      "test_hash2",
+			FromIndex:     100,
+			BundleSize:    100,
+			FromKey:       "100",
+			ToKey:         "199",
+			BundleSummary: "test_value2",
+		})
+
+		// ASSERT
+		pool, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 0)
+
+		// assert if bundle go finalized
+		Expect(pool.TotalBundles).To(Equal(uint64(1)))
+		Expect(pool.CurrentKey).To(Equal("99"))
+
+		fundingState, _ := s.App().FundersKeeper.GetFundingState(s.Ctx(), 0)
+
+		// assert total pool funds
+		Expect(s.App().FundersKeeper.GetTotalActiveFunding(s.Ctx(), fundingState.PoolId).String()).To(Equal(sdk.NewCoins(i.ACoin(90*i.T_KYVE), i.BCoin(48*i.T_KYVE)).String()))
+		Expect(fundingState.ActiveFunderAddresses).To(HaveLen(1))
+
+		funding, _ := s.App().FundersKeeper.GetFunding(s.Ctx(), i.ALICE, 0)
+
+		// assert individual funds
+		Expect(funding.Amounts.String()).To(Equal(sdk.NewCoins(i.ACoin(90*i.T_KYVE), i.BCoin(48*i.T_KYVE)).String()))
+		Expect(funding.TotalFunded.String()).To(Equal(sdk.NewCoins(i.ACoin(10*i.T_KYVE), i.BCoin(2*i.T_KYVE)).String()))
+
+		// assert individual balances
+		balanceAlice := s.GetCoinsFromAddress(i.ALICE)
+		Expect(balanceAlice.String()).To(Equal(initialBalanceAlice.Sub(i.ACoin(100*i.T_KYVE), i.BCoin(50*i.T_KYVE)).String()))
+	})
+
+	It("Produce a valid bundle with multiple funders and multiple coins", func() {
+		// ARRANGE
+		s.RunTxFundersSuccess(&funderstypes.MsgFundPool{
+			Creator:          i.ALICE,
+			PoolId:           0,
+			Amounts:          sdk.NewCoins(i.ACoin(100*i.T_KYVE), i.BCoin(50*i.T_KYVE)),
+			AmountsPerBundle: sdk.NewCoins(i.ACoin(10*i.T_KYVE), i.BCoin(2*i.T_KYVE)),
+		})
+
+		s.RunTxFundersSuccess(&funderstypes.MsgFundPool{
+			Creator:          i.BOB,
+			PoolId:           0,
+			Amounts:          sdk.NewCoins(i.BCoin(100*i.T_KYVE), i.CCoin(200*i.T_KYVE)),
+			AmountsPerBundle: sdk.NewCoins(i.BCoin(10*i.T_KYVE), i.CCoin(20*i.T_KYVE)),
+		})
+
+		s.CommitAfterSeconds(60)
+
+		s.RunTxBundlesSuccess(&bundletypes.MsgSubmitBundleProposal{
+			Creator:       i.VALADDRESS_0_A,
+			Staker:        i.STAKER_0,
+			PoolId:        0,
+			StorageId:     "y62A3tfbSNcNYDGoL-eXwzyV-Zc9Q0OVtDvR1biJmNI",
+			DataSize:      100,
+			DataHash:      "test_hash",
+			FromIndex:     0,
+			BundleSize:    100,
+			FromKey:       "0",
+			ToKey:         "99",
+			BundleSummary: "test_value",
+		})
+
+		s.RunTxBundlesSuccess(&bundletypes.MsgVoteBundleProposal{
+			Creator:   i.VALADDRESS_1_A,
+			Staker:    i.STAKER_1,
+			PoolId:    0,
+			StorageId: "y62A3tfbSNcNYDGoL-eXwzyV-Zc9Q0OVtDvR1biJmNI",
+			Vote:      1,
+		})
+
+		s.CommitAfterSeconds(60)
+
+		// ACT
+		s.RunTxBundlesSuccess(&bundletypes.MsgSubmitBundleProposal{
+			Creator:       i.VALADDRESS_1_A,
+			Staker:        i.STAKER_1,
+			PoolId:        0,
+			StorageId:     "P9edn0bjEfMU_lecFDIPLvGO2v2ltpFNUMWp5kgPddg",
+			DataSize:      100,
+			DataHash:      "test_hash2",
+			FromIndex:     100,
+			BundleSize:    100,
+			FromKey:       "100",
+			ToKey:         "199",
+			BundleSummary: "test_value2",
+		})
+
+		// ASSERT
+		pool, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 0)
+
+		// assert if bundle go finalized
+		Expect(pool.TotalBundles).To(Equal(uint64(1)))
+		Expect(pool.CurrentKey).To(Equal("99"))
+
+		fundingState, _ := s.App().FundersKeeper.GetFundingState(s.Ctx(), 0)
+
+		// assert total pool funds
+		Expect(s.App().FundersKeeper.GetTotalActiveFunding(s.Ctx(), fundingState.PoolId).String()).To(Equal(sdk.NewCoins(i.ACoin(90*i.T_KYVE), i.BCoin(138*i.T_KYVE), i.CCoin(180*i.T_KYVE)).String()))
+		Expect(fundingState.ActiveFunderAddresses).To(HaveLen(2))
+
+		fundingAlice, _ := s.App().FundersKeeper.GetFunding(s.Ctx(), i.ALICE, 0)
+		fundingBob, _ := s.App().FundersKeeper.GetFunding(s.Ctx(), i.BOB, 0)
+
+		// assert individual funds
+		Expect(fundingAlice.Amounts.String()).To(Equal(sdk.NewCoins(i.ACoin(90*i.T_KYVE), i.BCoin(48*i.T_KYVE)).String()))
+		Expect(fundingAlice.TotalFunded.String()).To(Equal(sdk.NewCoins(i.ACoin(10*i.T_KYVE), i.BCoin(2*i.T_KYVE)).String()))
+		Expect(fundingBob.Amounts.String()).To(Equal(sdk.NewCoins(i.BCoin(90*i.T_KYVE), i.CCoin(180*i.T_KYVE)).String()))
+		Expect(fundingBob.TotalFunded.String()).To(Equal(sdk.NewCoins(i.BCoin(10*i.T_KYVE), i.CCoin(20*i.T_KYVE)).String()))
+
+		// assert individual balances
+		balanceAlice := s.GetCoinsFromAddress(i.ALICE)
+		Expect(balanceAlice.String()).To(Equal(initialBalanceAlice.Sub(i.ACoin(100*i.T_KYVE), i.BCoin(50*i.T_KYVE)).String()))
+
+		balanceBob := s.GetCoinsFromAddress(i.BOB)
+		Expect(balanceBob.String()).To(Equal(initialBalanceBob.Sub(i.BCoin(100*i.T_KYVE), i.CCoin(200*i.T_KYVE)).String()))
+	})
+
+	It("Produce a valid bundle although the only funder can not pay for the full bundle reward", func() {
+		// ARRANGE
+		s.RunTxFundersSuccess(&funderstypes.MsgFundPool{
+			Creator:          i.ALICE,
+			PoolId:           0,
+			Amounts:          sdk.NewCoins(i.ACoin(100*i.T_KYVE), i.BCoin(50*i.T_KYVE)),
+			AmountsPerBundle: sdk.NewCoins(i.ACoin(10*i.T_KYVE), i.BCoin(100*i.T_KYVE)),
+		})
+
+		s.RunTxFundersSuccess(&funderstypes.MsgFundPool{
+			Creator:          i.BOB,
+			PoolId:           0,
+			Amounts:          sdk.NewCoins(i.BCoin(100*i.T_KYVE), i.CCoin(200*i.T_KYVE)),
+			AmountsPerBundle: sdk.NewCoins(i.BCoin(10*i.T_KYVE), i.CCoin(20*i.T_KYVE)),
+		})
+
+		s.CommitAfterSeconds(60)
+
+		s.RunTxBundlesSuccess(&bundletypes.MsgSubmitBundleProposal{
+			Creator:       i.VALADDRESS_0_A,
+			Staker:        i.STAKER_0,
+			PoolId:        0,
+			StorageId:     "y62A3tfbSNcNYDGoL-eXwzyV-Zc9Q0OVtDvR1biJmNI",
+			DataSize:      100,
+			DataHash:      "test_hash",
+			FromIndex:     0,
+			BundleSize:    100,
+			FromKey:       "0",
+			ToKey:         "99",
+			BundleSummary: "test_value",
+		})
+
+		s.RunTxBundlesSuccess(&bundletypes.MsgVoteBundleProposal{
+			Creator:   i.VALADDRESS_1_A,
+			Staker:    i.STAKER_1,
+			PoolId:    0,
+			StorageId: "y62A3tfbSNcNYDGoL-eXwzyV-Zc9Q0OVtDvR1biJmNI",
+			Vote:      1,
+		})
+
+		s.CommitAfterSeconds(60)
+
+		// ACT
+		s.RunTxBundlesSuccess(&bundletypes.MsgSubmitBundleProposal{
+			Creator:       i.VALADDRESS_1_A,
+			Staker:        i.STAKER_1,
+			PoolId:        0,
+			StorageId:     "P9edn0bjEfMU_lecFDIPLvGO2v2ltpFNUMWp5kgPddg",
+			DataSize:      100,
+			DataHash:      "test_hash2",
+			FromIndex:     100,
+			BundleSize:    100,
+			FromKey:       "100",
+			ToKey:         "199",
+			BundleSummary: "test_value2",
+		})
+
+		// ASSERT
+		pool, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 0)
+
+		// assert if bundle go finalized
+		Expect(pool.TotalBundles).To(Equal(uint64(1)))
+		Expect(pool.CurrentKey).To(Equal("99"))
+
+		fundingState, _ := s.App().FundersKeeper.GetFundingState(s.Ctx(), 0)
+
+		// assert total pool funds
+		Expect(s.App().FundersKeeper.GetTotalActiveFunding(s.Ctx(), fundingState.PoolId).String()).To(Equal(sdk.NewCoins(i.ACoin(90*i.T_KYVE), i.BCoin(90*i.T_KYVE), i.CCoin(180*i.T_KYVE)).String()))
+		Expect(fundingState.ActiveFunderAddresses).To(HaveLen(2))
+
+		fundingAlice, _ := s.App().FundersKeeper.GetFunding(s.Ctx(), i.ALICE, 0)
+		fundingBob, _ := s.App().FundersKeeper.GetFunding(s.Ctx(), i.BOB, 0)
+
+		// assert individual funds
+		Expect(fundingAlice.Amounts.String()).To(Equal(sdk.NewCoins(i.ACoin(90 * i.T_KYVE)).String()))
+		Expect(fundingAlice.TotalFunded.String()).To(Equal(sdk.NewCoins(i.ACoin(10*i.T_KYVE), i.BCoin(50*i.T_KYVE)).String()))
+		Expect(fundingBob.Amounts.String()).To(Equal(sdk.NewCoins(i.BCoin(90*i.T_KYVE), i.CCoin(180*i.T_KYVE)).String()))
+		Expect(fundingBob.TotalFunded.String()).To(Equal(sdk.NewCoins(i.BCoin(10*i.T_KYVE), i.CCoin(20*i.T_KYVE)).String()))
+
+		// assert individual balances
+		balanceAlice := s.GetCoinsFromAddress(i.ALICE)
+		Expect(balanceAlice.String()).To(Equal(initialBalanceAlice.Sub(i.ACoin(100*i.T_KYVE), i.BCoin(50*i.T_KYVE)).String()))
+
+		balanceBob := s.GetCoinsFromAddress(i.BOB)
+		Expect(balanceBob.String()).To(Equal(initialBalanceBob.Sub(i.BCoin(100*i.T_KYVE), i.CCoin(200*i.T_KYVE)).String()))
 	})
 })
