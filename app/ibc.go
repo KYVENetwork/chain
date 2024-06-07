57153a6db3f5727cc76d75a6ed71a86074c9788c
Raphael <rapha@kyve.network>
2024-06-07 11:24:57 +0200

    chore: reduce ibc usage and add ibc tests (#193)

--- app/ibc.go
+++ app/ibc.go
@@ -12,17 +12,6 @@ import (
 	"github.com/cosmos/ibc-go/modules/capability"
 	capabilitykeeper "github.com/cosmos/ibc-go/modules/capability/keeper"
 	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
-	icamodule "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts"
-	icacontroller "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller"
-	icacontrollerkeeper "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/keeper"
-	icacontrollertypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/types"
-	icahost "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host"
-	icahostkeeper "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/keeper"
-	icahosttypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/types"
-	icatypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/types"
-	ibcfee "github.com/cosmos/ibc-go/v8/modules/apps/29-fee"
-	ibcfeekeeper "github.com/cosmos/ibc-go/v8/modules/apps/29-fee/keeper"
-	ibcfeetypes "github.com/cosmos/ibc-go/v8/modules/apps/29-fee/types"
 	ibctransfer "github.com/cosmos/ibc-go/v8/modules/apps/transfer"
 	ibctransferkeeper "github.com/cosmos/ibc-go/v8/modules/apps/transfer/keeper"
 	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
@@ -44,9 +33,6 @@ func (app *App) registerIBCModules() {
 		storetypes.NewKVStoreKey(capabilitytypes.StoreKey),
 		storetypes.NewKVStoreKey(ibcexported.StoreKey),
 		storetypes.NewKVStoreKey(ibctransfertypes.StoreKey),
-		storetypes.NewKVStoreKey(ibcfeetypes.StoreKey),
-		storetypes.NewKVStoreKey(icahosttypes.StoreKey),
-		storetypes.NewKVStoreKey(icacontrollertypes.StoreKey),
 		storetypes.NewMemoryStoreKey(capabilitytypes.MemStoreKey),
 		storetypes.NewTransientStoreKey(paramstypes.TStoreKey),
 	); err != nil {
@@ -58,8 +44,6 @@ func (app *App) registerIBCModules() {
 	keyTable.RegisterParamSet(&ibcconnectiontypes.Params{})
 	app.ParamsKeeper.Subspace(ibcexported.ModuleName).WithKeyTable(keyTable)
 	app.ParamsKeeper.Subspace(ibctransfertypes.ModuleName).WithKeyTable(ibctransfertypes.ParamKeyTable())
-	app.ParamsKeeper.Subspace(icacontrollertypes.SubModuleName).WithKeyTable(icacontrollertypes.ParamKeyTable())
-	app.ParamsKeeper.Subspace(icahosttypes.SubModuleName).WithKeyTable(icahosttypes.ParamKeyTable())
 
 	// add capability keeper and ScopeToModule for ibc module
 	app.CapabilityKeeper = capabilitykeeper.NewKeeper(
@@ -71,8 +55,6 @@ func (app *App) registerIBCModules() {
 	// add capability keeper and ScopeToModule for ibc module
 	scopedIBCKeeper := app.CapabilityKeeper.ScopeToModule(ibcexported.ModuleName)
 	scopedIBCTransferKeeper := app.CapabilityKeeper.ScopeToModule(ibctransfertypes.ModuleName)
-	scopedICAControllerKeeper := app.CapabilityKeeper.ScopeToModule(icacontrollertypes.SubModuleName)
-	scopedICAHostKeeper := app.CapabilityKeeper.ScopeToModule(icahosttypes.SubModuleName)
 
 	// Create IBC keeper
 	app.IBCKeeper = ibckeeper.NewKeeper(
@@ -92,19 +74,12 @@ func (app *App) registerIBCModules() {
 	govRouter := govv1beta1.NewRouter()
 	govRouter.AddRoute(govtypes.RouterKey, govv1beta1.ProposalHandler)
 
-	app.IBCFeeKeeper = ibcfeekeeper.NewKeeper(
-		app.appCodec, app.GetKey(ibcfeetypes.StoreKey),
-		app.IBCKeeper.ChannelKeeper, // may be replaced with IBC middleware
-		app.IBCKeeper.ChannelKeeper,
-		app.IBCKeeper.PortKeeper, app.AccountKeeper, app.BankKeeper,
-	)
-
 	// Create IBC transfer keeper
 	app.IBCTransferKeeper = ibctransferkeeper.NewKeeper(
 		app.appCodec,
 		app.GetKey(ibctransfertypes.StoreKey),
 		app.GetSubspace(ibctransfertypes.ModuleName),
-		app.IBCFeeKeeper,
+		app.IBCKeeper.ChannelKeeper,
 		app.IBCKeeper.ChannelKeeper,
 		app.IBCKeeper.PortKeeper,
 		app.AccountKeeper,
@@ -113,49 +88,14 @@ func (app *App) registerIBCModules() {
 		authtypes.NewModuleAddress(govtypes.ModuleName).String(),
 	)
 
-	// Create interchain account keepers
-	app.ICAHostKeeper = icahostkeeper.NewKeeper(
-		app.appCodec,
-		app.GetKey(icahosttypes.StoreKey),
-		app.GetSubspace(icahosttypes.SubModuleName),
-		app.IBCFeeKeeper, // use ics29 fee as ics4Wrapper in middleware stack
-		app.IBCKeeper.ChannelKeeper,
-		app.IBCKeeper.PortKeeper,
-		app.AccountKeeper,
-		scopedICAHostKeeper,
-		app.MsgServiceRouter(),
-		authtypes.NewModuleAddress(govtypes.ModuleName).String(),
-	)
-	app.ICAControllerKeeper = icacontrollerkeeper.NewKeeper(
-		app.appCodec,
-		app.GetKey(icacontrollertypes.StoreKey),
-		app.GetSubspace(icacontrollertypes.SubModuleName),
-		app.IBCFeeKeeper, // use ics29 fee as ics4Wrapper in middleware stack
-		app.IBCKeeper.ChannelKeeper,
-		app.IBCKeeper.PortKeeper,
-		scopedICAControllerKeeper,
-		app.MsgServiceRouter(),
-		authtypes.NewModuleAddress(govtypes.ModuleName).String(),
-	)
 	app.GovKeeper.SetLegacyRouter(govRouter)
 
 	// Create IBC modules with ibcfee middleware
-	transferIBCModule := ibcfee.NewIBCMiddleware(ibctransfer.NewIBCModule(app.IBCTransferKeeper), app.IBCFeeKeeper)
-
-	// integration point for custom authentication modules
-	var noAuthzModule porttypes.IBCModule
-	icaControllerIBCModule := ibcfee.NewIBCMiddleware(
-		icacontroller.NewIBCMiddleware(noAuthzModule, app.ICAControllerKeeper),
-		app.IBCFeeKeeper,
-	)
-
-	icaHostIBCModule := ibcfee.NewIBCMiddleware(icahost.NewIBCModule(app.ICAHostKeeper), app.IBCFeeKeeper)
+	transferIBCModule := ibctransfer.NewIBCModule(app.IBCTransferKeeper)
 
 	// Create static IBC router, add transfer route, then set and seal it
 	ibcRouter := porttypes.NewRouter().
-		AddRoute(ibctransfertypes.ModuleName, transferIBCModule).
-		AddRoute(icacontrollertypes.SubModuleName, icaControllerIBCModule).
-		AddRoute(icahosttypes.SubModuleName, icaHostIBCModule)
+		AddRoute(ibctransfertypes.ModuleName, transferIBCModule)
 
 	// this line is used by starport scaffolding # ibc/app/module
 
@@ -163,15 +103,11 @@ func (app *App) registerIBCModules() {
 
 	app.ScopedIBCKeeper = scopedIBCKeeper
 	app.ScopedIBCTransferKeeper = scopedIBCTransferKeeper
-	app.ScopedICAHostKeeper = scopedICAHostKeeper
-	app.ScopedICAControllerKeeper = scopedICAControllerKeeper
 
 	// register IBC modules
 	if err := app.RegisterModules(
 		ibc.NewAppModule(app.IBCKeeper),
 		ibctransfer.NewAppModule(app.IBCTransferKeeper),
-		ibcfee.NewAppModule(app.IBCFeeKeeper),
-		icamodule.NewAppModule(&app.ICAControllerKeeper, &app.ICAHostKeeper),
 		capability.NewAppModule(app.appCodec, *app.CapabilityKeeper, false),
 		ibctm.AppModule{},
 		solomachine.AppModule{},
@@ -187,8 +123,6 @@ func RegisterIBC(registry cdctypes.InterfaceRegistry) map[string]appmodule.AppMo
 	modules := map[string]appmodule.AppModule{
 		ibcexported.ModuleName:      ibc.AppModule{},
 		ibctransfertypes.ModuleName: ibctransfer.AppModule{},
-		ibcfeetypes.ModuleName:      ibcfee.AppModule{},
-		icatypes.ModuleName:         icamodule.AppModule{},
 		capabilitytypes.ModuleName:  capability.AppModule{},
 		ibctm.ModuleName:            ibctm.AppModule{},
 		solomachine.ModuleName:      solomachine.AppModule{},
---
+++ interchaintest/ibc/ibc_utils_test.go
@@ -0,0 +1,89 @@
+package relayer_test
+
+import (
+	"cosmossdk.io/math"
+	"encoding/json"
+	"github.com/KYVENetwork/chain/app"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdktestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/icza/dyno"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+	"github.com/strangelove-ventures/interchaintest/v8"
+	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"strconv"
+)
+
+const (
+	uidGid = "1025:1025"
+)
+
+func encodingConfig() *sdktestutil.TestEncodingConfig {
+	cfg := sdktestutil.TestEncodingConfig{}
+	a := app.Setup()
+
+	cfg.Codec = a.AppCodec()
+	cfg.TxConfig = authtx.NewTxConfig(a.AppCodec(), authtx.DefaultSignModes)
+	cfg.InterfaceRegistry = a.InterfaceRegistry()
+	cfg.Amino = a.LegacyAmino()
+
+	return &cfg
+}
+
+func mainnetChainSpec(numValidators int, numFullNodes int) *interchaintest.ChainSpec {
+	return &interchaintest.ChainSpec{
+		NumValidators: &numValidators,
+		NumFullNodes:  &numFullNodes,
+		ChainConfig: ibc.ChainConfig{
+			Type:           "cosmos",
+			Name:           "kyve",
+			ChainID:        "kyve-1",
+			Bin:            "kyved",
+			Bech32Prefix:   "kyve",
+			Denom:          "ukyve",
+			GasPrices:      "0.02ukyve",
+			GasAdjustment:  5,
+			TrustingPeriod: "112h",
+			NoHostMount:    false,
+			EncodingConfig: encodingConfig(),
+			ModifyGenesis:  ModifyGenesis,
+			Images: []ibc.DockerImage{{
+				Repository: "kyve",
+				Version:    "local",
+				UidGid:     uidGid,
+			}},
+		},
+	}
+}
+
+func ModifyGenesis(config ibc.ChainConfig, genbz []byte) ([]byte, error) {
+	genesis := make(map[string]interface{})
+	_ = json.Unmarshal(genbz, &genesis)
+
+	teamSupply := math.NewInt(165_000_000_000_000)
+	balances, _ := dyno.GetSlice(genesis, "app_state", "bank", "balances")
+	balances = append(balances, bankTypes.Balance{
+		Address: "kyve1e29j95xmsw3zmvtrk4st8e89z5n72v7nf70ma4",
+		Coins:   sdk.NewCoins(sdk.NewCoin(config.Denom, teamSupply)),
+	})
+	_ = dyno.Set(genesis, balances, "app_state", "bank", "balances")
+	totalSupply, _ := dyno.GetSlice(genesis, "app_state", "bank", "supply")
+
+	// update total supply
+	coin := totalSupply[0].(map[string]interface{})
+	amountStr := coin["amount"].(string)
+	amount, _ := strconv.Atoi(amountStr)
+	totalSupply[0] = sdk.NewCoin(config.Denom, math.NewInt(int64(amount)+teamSupply.Int64()))
+	_ = dyno.Set(genesis, totalSupply, "app_state", "bank", "supply")
+
+	newGenesis, _ := json.Marshal(genesis)
+	return newGenesis, nil
+}
+
+func BeBetween(min, max interface{}) types.GomegaMatcher {
+	return SatisfyAll(
+		BeNumerically(">", min),
+		BeNumerically("<", max))
+}
